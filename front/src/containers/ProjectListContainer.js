e7fde5be54e7d7ae6fb87d463d09c78fa286e704
Bruno Belorgey <bruno.belorgey@laposte.net>
2018-12-05 14:25:46 +0100

    correction src/containers/ProjectListContainer.js

--- front/src/containers/ProjectListContainer.js
+++ front/src/containers/ProjectListContainer.js
@@ -11,27 +11,36 @@ class ProjectListContainer extends Component {
       error: null,
       initiative: [],
       mission: [],
-      evenement: []
+      evenement: [],
+      loaded:false
     }
   }
 
   componentDidMount() {
-      // recup path Route d'appel
-      let regex = /\//;
-      const projecType = this.props.match.path.replace(regex, '');
-      this.fetchProject(projecType);
+    // recup path Route d'appel dans les props
+    let regex = /\//;
+    const projecType = this.props.match.path.replace(regex, '');
+    this.setState({loaded:false});
+    this.fetchProject(projecType);
   }
 
   fetchProject(projecType) {
 
     axios.get(`/api/project/${projecType}`)
       .then(res => res.data)
-      .then(projects => this.setState({ [projecType]: projects }))
+      .then(projects => this.setState({ [projecType]: projects,loaded:true }))
       .catch(error => this.setState({ error }))
   }
 
   render() {
+    // recup path Route d'appel dans les props
+    let regex = /\//;
+    const projecType = this.props.match.path.replace(regex, '');
     const projects = this.state[projecType];
+    const {loaded} = this.state;
+    if(loaded===false){
+      
+    }
     return (
       <div>
         {projects.length > 0
