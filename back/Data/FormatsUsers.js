c333118eab3e67be7a126c62ec4e1eb6f492bcd6
Julien Chapron <julien.chapron@protonmail.ch>
2019-01-04 14:18:12 +0100

    add json of users

--- back/Data/FormatsUsers.js
+++ back/Data/FormatsUsers.js
@@ -1,15 +1,34 @@
-const data = require('./users.json');
+const data = require('./dataUsers.json');
 const { results } = data;
 
+
+
+/* LOAD DATA INFILE '/var/lib/mysql-files/users.csv' 
+INTO TABLE users 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+(lastname, firstname, connext, email, password, presentation, picture, skill )
+SET ID = NULL; */
+
+// algo pour recuperer les clés d'un fichier json de randomUser
 const users = results.map(result => {
   const { name, gender, email, login, picture } = result;
   const { first, last, title } = name;
   const { username, password } = login;
-  const { medium } = picture;
+
+  const { large } = picture;
   // ici reformater pour avoir les mêmes champs que dans la DB
-  return {last, first, username, email, password, gender, medium, title };
+  return {last, first, username, email, password, gender, large, title };
 });
 
-console.log(users);
+const usersJSON = JSON.stringify(users)
+
+console.log(usersJSON)
 // importer db.js
 // faire une boucle: pour chaque user du tableau, db.query('INSERT INTO user SET ?' , user);
+
+// sauver en JSON un tableau
+// 1. const usersJSON = JSON.stringify(users)
+// 2. fs.writeFileSync('filename.json', usersJSON)
