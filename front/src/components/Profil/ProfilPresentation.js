28682ae3a99b8563f73a4360ca4fc326152f5880
Julien Chapron <julien.chapron@protonmail.ch>
2019-01-07 15:52:15 +0100

    chnage route profil/:id/mission,initiative,favorite => profil/mission,initiative,favorite, add variable to read data from mysql, problem with to read data from favoriteList and projectList to profil, change all passwords of contributor in phpmyadmin => toto => token to read all email adress in connect form

--- front/src/components/Profil/ProfilPresentation.js
+++ front/src/components/Profil/ProfilPresentation.js
@@ -14,11 +14,11 @@ const ProfilPresentation = ({ user }) => ({
               <img className="w-100 rounded-circle" src={user.picture} alt={user.picture} />
             </Col>
             <Col lg="8" className="mt-5">
-              {/* <Subtitle className="font-weight-bold">
+              <Subtitle className="font-weight-bold">
                 {user.lastname.charAt(0).toUpperCase() + user.lastname.slice(1)}
                 &nbsp;{user.firstname.charAt(0).toUpperCase() + user.firstname.slice(1)}
                 <i style={{ fontSize: '2vh' }} className="fas fa-edit fa-fw mr-2 ml-2" />
-              </Subtitle> */}
+              </Subtitle>
               <Text
                 className="m-1"
               >
@@ -48,9 +48,9 @@ const ProfilPresentation = ({ user }) => ({
               <i style={{ fontSize: '2vh' }} className="fas fa-edit fa-fw mr-2 ml-2" />
             </Text>
           </Row>
-          {/* <Row className="mt-2 pb-5">
+          <Row className="mt-2 pb-5">
             {user.skill.split(',').map((skill, key) => <Competence key={key}>{skill}</Competence>)}
-          </Row> */}
+          </Row>
         </Container>
       </StyledContainer>
     );
--- front/src/containers/ProfilListContainer.js
+++ front/src/containers/ProfilListContainer.js
@@ -20,11 +20,11 @@ class ProfilListContainer extends Component {
     this.state = {
       user: null,
       error: null,
-      projects: [],
+      projects: null,
       evenement: [],
       projecType: '',
       id: '',
-      userId: ''
+      test: ''
     };
   }
 
@@ -37,28 +37,29 @@ class ProfilListContainer extends Component {
 
   componentDidMount() {
     const firstAxios = this.props;
-    const { userId } = this.state;
-    axios.get(`/api/profil/${userId}`)
+    const { test } = this.props;
+    console.log(test)
+    axios.get(`/api/profil/${test}`)
       .then(res => res.data)
       .then(user => this.setState({ user }))
       .catch(error => this.setState({ error }));
-    const projecType = firstAxios.match.path.replace(`/profil/${userId}/`, '');
+    const projecType = firstAxios.match.path.replace(`/profil/${test}/`, '');
     this.fetchProjecType(projecType);
   }
 
   componentDidUpdate(prevProps) {
-    const { userId } = this.state;
+    const { test } = this.props;
     const secondAxios = this.props;
-    const prevProjecType = prevProps.match.path.replace(`/profil/${userId}/`, '');
-    const projecType = secondAxios.match.path.replace(`/profil/${userId}/`, '');
+    const prevProjecType = prevProps.match.path.replace(`/profil/`, '');
+    const projecType = secondAxios.match.path.replace(`/profil/`, '');
     if (prevProjecType !== projecType) {
       this.fetchProjecType(projecType);
     }
   }
 
   fetchProjecType(projecType) {
-    const { userId } = this.state;
-    axios.get(`/api/profil/${userId}/${projecType}`)
+    const { test } = this.props;
+    axios.get(`/profil/${test}/${projecType}`)
       .then(res => res.data)
       .then(projects => this.setState({ [projecType]: projects, loaded: true }))
       .catch(error => this.setState({ error }));
@@ -66,24 +67,23 @@ class ProfilListContainer extends Component {
 
   render() {
     const matchPath = this.props;
-    const {userId} = this.props;
+    const {test} = this.props;
     const {user} = this.state;
-    const projecType = matchPath.match.path.substr(12);
-    console.log(projecType)
+    const projecType = matchPath.match.path.substr(8);
     const projects = this.state[projecType];
+    console.log(projecType);
     const ListComponent = componentMap[projecType];
-    console.log(projects);
+    console.log(ListComponent)
     return (
       <Container fluid style={{ marginTop: '150px' }}>
-        {userId
+        {user
           ? <div className="mt-5 mb-5"><ProfilPresentation user={user} /><ListComponent projects={projects} /></div>
-          : <div className="p-5 text-center"><Container className="bg-warning p-5 rounded"><p>Page impossible à afficher</p></Container></div>
+          : <div className="p-5 text-center"><Container className="bg-warning p-5 rounded"><p>Erreur 404 => Page impossible à afficher</p></Container></div>
         }
       </Container>
     );
   }
 }
-const mapStateToProps = state => { return { test: state.auth }; };
+const mapStateToProps = state => { return { test: state.auth.user.id }; };
 
 export default connect(mapStateToProps)(ProfilListContainer);
-
