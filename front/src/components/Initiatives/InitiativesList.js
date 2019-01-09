f65efc999783a93d77869ec7c2444cdb22cbfd69
Bruno Belorgey <bruno.belorgey@laposte.net>
2019-01-09 09:42:55 +0100

    recherche

--- front/src/components/Initiatives/InitiativesList.js
+++ front/src/components/Initiatives/InitiativesList.js
@@ -12,7 +12,7 @@ class InitiativesList extends Component {
     super(props);
     this.state = {
       inputSearch: '',
-      id: 0
+      id: [0]
     }
     this.nameFilter = this.nameFilter.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
@@ -22,25 +22,34 @@ class InitiativesList extends Component {
   handleSearch(e) {
     let inputSearch = e.target.value;
     let { id } = this.state;
-    if (inputSearch === '') id = 0;
+    if (inputSearch === '') id = [0];
     this.setState({ inputSearch, id });
   }
 
   searchId() {
-    const { users } = this.props;
+    const { users, initiative } = this.props;
     const { inputSearch } = this.state;
-    let id = -1;
-
-    // regex pour une partie du nom seulement
+    let id = [];
+    // regex pour une partie du nom seulement ou email
     let regex = new RegExp("\\w*"+inputSearch+"\\w*", "gi");
     // console.log(regex.exec(users[i].firstname));
-
     for (let i = 0; i < users.length; i++) {
-      if (users[i].firstname === inputSearch) {
-        id = users[i].id;
+      if (regex.test(users[i].firstname)||regex.test(users[i].email)) {
+        id = [...id,users[i].id];
         break;
       }
     }
+    // recherche dans project sur name, summuary, description, sponsor et wanted skill
+    initiative.map(item => {
+      let allfield = Object.values(item).join(' ');
+      console.log('allfield',allfield);
+      if (regex.test(allfield)) {
+        id = [...id,item.userId];
+        console.log('id',id);
+    }
+  })
+    console.log('id',id);
+    if(id.length===0) id=[-1];
     this.setState({ id });
   }
 
@@ -102,14 +111,15 @@ class InitiativesList extends Component {
                   projects.filter(elt => {
                     let { id } = this.state;
                     // console.log(id,elt.userId);
-                    if (id === 0) {
+                    if (id[0] === 0) {
                       return true;
                     } else {
-                      if (elt.userId === id) {
-                        return true;
-                      } else {
-                        return false;
+                      for(let i=0;i<id.length;i++){
+                        if (elt.userId === id[i]) {
+                          return true;
+                        } 
                       }
+                        return false;
                     }
                   }).map((initiative, index) =>
                     <InitiativeItem
@@ -130,7 +140,8 @@ class InitiativesList extends Component {
 const mapStateToProps = state => ({
   users: state.users.users,
   loading: state.users.loading,
-  error: state.users.error
+  error: state.users.error,
+  initiative: state.project.initiative
 });
 
 const mapDispatchToProps = {
