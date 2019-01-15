1668498a9eb631070b88d029cb2364af9d49e14e
Eva Spessotto <eva@spessotto.com>
2019-01-15 14:38:23 +0100

    Modif carte initiative

--- front/src/components/Initiatives/InitiativeItem.js
+++ front/src/components/Initiatives/InitiativeItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../../css/initiativeItem.scss';
+import moment from 'moment';
 import { Link } from 'react-router-dom';
 import { Text, SubtitleLink, InitiativeCard } from '../../data/styledComponents';
 import { Container, Row, Col } from 'reactstrap';
 import InitiativeReward from './InitiativeReward';
 
-const InitiativeItem = ({ id, logo, name, projectType, sponsors, prizes }) => (
+const InitiativeItem = ({ id, logo, name, projectType, sponsors, prizes, startDate, endDate }) => (
   
   <InitiativeCard className="mb-3" id="initiative-item">
     <Container>
@@ -17,16 +18,29 @@ const InitiativeItem = ({ id, logo, name, projectType, sponsors, prizes }) => (
         <Col className="mt-3">
           <Container fluid>
             <Row>
+              <Text className="mr-4">
+                <i className="fas fa-calendar-alt fa-fw icons" />
+                {
+                  moment(startDate).subtract(10, 'days').calendar()
+                }
+              </Text>
+              <Text>
+                <i className="fas fa-calendar-alt fa-fw icons" />
+                {
+                  moment(endDate).subtract(10, 'days').calendar()
+                }
+              </Text>
+            </Row>
+            <Row className="my-2">
               <Col className="p-0">
-              <Link to={`/${projectType}/${id}`}>
-                <SubtitleLink>{name}</SubtitleLink>
-              </Link>
-                
+                <Link to={`/${projectType}/${id}`}>
+                  <SubtitleLink>{name}</SubtitleLink>
+                </Link>
               </Col>
             </Row>
             <Row>
               <Col className="p-0">
-                <Text className="mt-3">
+                <Text>
                   Description
                   <i className="fas fa-sort-down fa-fw ml-1"></i>
                 </Text>
