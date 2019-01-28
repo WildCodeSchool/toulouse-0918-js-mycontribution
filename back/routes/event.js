f3c41976d8a2474eceef7d1b58b507de4e9f39f1
Benoît Hubert <benoithubert@gmail.com>
2019-01-28 15:11:30 +0100

    Change ordre tri des évènements (#120)

--- back/routes/event.js
+++ back/routes/event.js
@@ -6,7 +6,7 @@ const db = require('../conf');
 router.get('/',(req, res) => {
   const { projectId } = req.query;
   const where = projectId ? 'WHERE projectId = ?' : '';
-  db.query(`select * from event ${where} ORDER BY eventDate ASC`, [projectId], (err, event) => {
+  db.query(`select * from event ${where} ORDER BY eventDate DESC`, [projectId], (err, event) => {
     if(err) {
       return res.status(500).json({ error: err.message });
     }
